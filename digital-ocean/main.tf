1c8df1e5cabdd45725142378a0d4e2c340bcb427
Arsenio Aguirre <a_aguirre117@hotmail.com>
2020-08-20 12:27:41 -0300

    Terraform upgrade to 0.13 - Main.tf modifications

--- digital-ocean/main.tf
+++ digital-ocean/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "1.22.2"
+    }
+  }
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
--- gcp/main.tf
+++ gcp/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.35.0"
+    }
+  }
+}
+
 // Configure the Google Cloud provider
 provider "google" {
     credentials = file("../credentials/pentest.json")
